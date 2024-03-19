21fe5b9a92fa57325611475649704f69e3145103
samson <d.kroeger@bellmountain-media.de>
2024-03-19 14:06:08 -0400

    first links

--- install.sh
+++ install.sh
@@ -1,2 +1,15 @@
 #/bin/bash script to install the app
 echo "Installing the app"
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/lazyvim ~/.config/lazyvim
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/xkb ~/.config/xkb
+ln -s ~/dotfiles/kitty ~/.config/kitty
+#
+# link the ff profiles
+# ln -s ~/dotfiles/firefox/ ~/.mozilla/firefox/archives
+# ln -s ~/dotfiles/firefox/profiles.ini ~/.mozilla/firefox/profiles
+# 
+# link the scripts
+# ln -s ~/dotfiles/scripts/ ~/scripts
+
